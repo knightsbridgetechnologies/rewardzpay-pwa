d99aaa38742cb9d4988e1acde7b357133356a4aa
Randika94 <randika.s@knightsbridge.tech>
2024-05-03 18:16:06 +0530

    modified reports ui

--- src/app/report/page.js
+++ src/app/report/page.js
@@ -10,7 +10,8 @@ import { FaArrowLeft, FaHome, FaShareAlt, FaSearch, FaFilter, FaPowerOff  } from
 export default  function page() {
 
     const router = useRouter();
-
+    const [isSearchOpen, setIsSearchOpen] = useState(false);
+    
     async function logoutUser() {
         await logout()
         router.push("/")
@@ -19,6 +20,12 @@ export default  function page() {
     async function gotoHome(){
         router.push("/home")
     }
+
+   
+
+    const toggleSearch = () => {
+        setIsSearchOpen(!isSearchOpen);
+    };
 
     async function shareReportData(){
         const response = await emailReportData(params)
@@ -55,10 +62,10 @@ export default  function page() {
                 <div className="grid grid-cols-2 gap-2">
                     <div className="flex items-center px-3 text-left text-white font-medium text-xl">
                         <button onClick={gotoHome}><FaArrowLeft className='mr-2 font-semibold cursor-pointer'/></button>    
-                        Redemption Report
+                        <span className='text-nowrap'>Redemption Report</span>
                     </div>
                     
-                    <div className="px-3 text-right font-sm font-normal text-white mt-2">
+                    <div className="flex items-center justify-end px-3 text-right font-sm font-normal text-white">
                         
                         {/* <input type='text' 
                             className='border rounded-lg  p-1 border-black-700 bg-primary text-white' 
@@ -66,16 +73,27 @@ export default  function page() {
                             onChange={(e) => setParams({...params,voucher_reference: e.target.value})} 
                             placeholder='Search by reference no'
                         /> */}
-                        <button onClick={gotoHome} href="/home" className='text-white mr-7 pl-1'><FaSearch /></button>
-                        <button onClick={gotoHome} href="/home" className='text-white mr-7 pl-1'><FaFilter /></button>
-                        <button  onClick={shareReportData} className='text-white mr-7 pl-1'><FaShareAlt /></button >                
+
+                        <div className="flex">
+                            <div className="mt-2">
+                                <button id="searchBtn" className="text-white mr-4 pl-1" onClick={toggleSearch}>
+                                    <FaSearch className='text-xl' />
+                                </button>
+                                {isSearchOpen && (
+                                    <input type="text" placeholder="Search..." className="absolute top-0 right-0 mt-8 p-2 rounded border border-gray-300"/>
+                                )}
+                            </div>
+                            <button onClick={gotoHome} href="/home" className='text-white mr-4 pl-1'><FaFilter className='text-xl' /></button>
+                            <button  onClick={shareReportData} className='text-white mr-4 pl-1'><FaShareAlt className='text-xl' /></button > 
+                        </div>
+                                       
                     </div>
                 </div>
                
             </nav>
             
-            <div className="flex justify-center items-center">
-                <div className="p-6 bg-white rounded-md">
+            <div className="md-xl:flex md-xl:justify-center md-xl:items-center">
+                <div className="md-xl:p-6 sm:p-2 bg-white rounded-md">
                     {data.error == false ? 
                         data.data.map((voucher,index) => <ReportCard key={index} voucher={voucher}/> )                    
                     : 
