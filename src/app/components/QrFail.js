e78a77a1a088db771377a83d03ec2488b9ae41cc
Randika94 <randika.s@knightsbridge.tech>
2024-05-06 14:27:18 +0530

    modified

--- src/app/components/QrFail.js
+++ src/app/components/QrFail.js
@@ -11,7 +11,7 @@ const QrFail = ({props}) => {
                     </div>
                     <h2 className="text-2xl font-bold">{props.message}</h2>
 
-                    <a href="/home" className="inline-block bg-white-500 text-black rounded-full border border-green-600 px-4 py-2">
+                    <a href="/home" className="inline-block bg-secondary text-white rounded mt-2 px-4 py-2">
                         Home
                     </a>                   
                 </div>
